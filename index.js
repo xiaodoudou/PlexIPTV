fcce466b38a2970ed10d5ec8033f4c7a2ca5d98c
Xiaodoudou <edouard.durand@gmail.com>
2018-05-13 18:58:43 +0800

    fix plex crash / empty channels list

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ class Server {
               defaultChannel++
             }
             const line = {
-              channel,
+              channel: `${channel}`,
               name,
               url
             }
